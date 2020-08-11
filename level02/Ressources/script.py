3edb95befb811848d99bfe7888267bdb2601a262
Piriou SCOTT <pscott@student.42.fr>
2020-08-11 12:45:20 +0200

    Update ressources and walkthrough

--- level02/Ressources/script.py
+++ level02/Ressources/script.py
@@ -1,4 +1,6 @@
-with open("notes", 'r') as f:
+#!/usr/bin/python
+
+with open("hexa", 'r') as f:
     nums = f.read().split('\n')
     res = "".join([bytearray.fromhex(num).decode()[::-1] for num in nums])
-    print(res
+    print(res)
